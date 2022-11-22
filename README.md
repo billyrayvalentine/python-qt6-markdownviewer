bd4511f131fab5e75dade70be5e493a8a6179ebd
billyrayvalentine <ben@cerium.org>
2022-11-22 20:03:07 +0000

    feat: Add logging, commandline args and update README

--- README.md
+++ README.md
@@ -1,6 +1,56 @@
 # MKV - Markdown Viewer
-A very simple pyside6 app to view Markdown using the power of QT6
+A very simple PySide6 app to view Markdown using the power of QT6.
 
 Ever wanted a simple app just to validate and view your Markdown before you
 commit it for all to see?  Well I did, so I wrote this initially in about half a
 day.
+
+Known to work with:
+
+Python 3.9
+
+PySide6 6.4.0.1
+
+# Install
+Install the pip package ```PySide6```
+
+And run:
+```python3.9 mkv.py```
+
+# Usage
+```shell
+usage: mkv.py [-h] [-d {CRITICAL,ERROR,WARNING,INFO,DEBUG}] [file]
+
+MKV - MarkDownViewer
+
+positional arguments:
+  file                  file to open
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d {CRITICAL,ERROR,WARNING,INFO,DEBUG}, --debug {CRITICAL,ERROR,WARNING,INFO,DEBUG}
+                        debug level
+```
+
+## Mouse and Key Bindings
+The read-only mode QTextEdit key and mouse bindings can be used
+(https://doc.qt.io/qt-6/qtextedit.html#read-only-key-bindings)
+
+i.e.
+
+| Keypresses | Action                                                        |
+| ---------- | ------------------------------------------------------------- |
+| Up         | Moves one line up.                                            |
+| Down       | Moves one line down.                                          |
+| Left       | Moves one character to the left.                              |
+| Right      | Moves one character to the right.                             |
+| PageUp     | Moves one (viewport) page up.                                 |
+| PageDown   | Moves one (viewport) page down.                               |
+| Home       | Moves to the beginning of the text.                           |
+| End        | Moves to the end of the text.                                 |
+| Alt+Wheel  | Scrolls the page horizontally (the Wheel is the mouse wheel). |
+| Ctrl+Wheel | Zooms the text.                                               |
+| Ctrl+A     | Selects all text.                                             |
+
+# Screenshot
+![screenhot_linux](misc/screenshot_linux)
--- mkv.py
+++ mkv.py
@@ -1,24 +1,40 @@
 import sys
 import os
+import argparse
+import signal
+import logging
 from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
 from PySide6.QtGui import QTextDocument
 from PySide6.QtCore import QFile, QUrl
 from ui_markdownviewer import Ui_MainWindow
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+class MarkDownViewer(QMainWindow, Ui_MainWindow):
+    def __init__(self, file: QUrl = None, log_level: str = "WARNING"):
+        super().__init__()
+        self.setupUi(self)
 
-        self.ui.action_About_Markdown_Viewer.triggered.connect(self.showinfo)
-        self.ui.action_Open.triggered.connect(self.openfile)
+        self.action_About_Markdown_Viewer.triggered.connect(self.showinfo)
+        self.action_Open.triggered.connect(self.openfile)
+
+        # Setup logging
+        logging.basicConfig(
+            format="%(asctime)s %(levelname)s %(message)s",
+            datefmt="%Y/%m/%d %H:%M:%S",
+            level=log_level,
+        )
+
+        self.show()
+
+        # Launch with a file if file is not None
+        if file:
+            logging.info(f"Starting with file: {file}")
+            self.showfile(file)
 
     # Slots
     def showinfo(self):
         """Show the about application dialog"""
-        QMessageBox.information(
+        QMessageBox.about(
             self,
             "About MKV (MarkDown Viewer)",
             "MKV (MarkDown Viewer) \n\
@@ -33,27 +49,49 @@ class MainWindow(QMainWindow):
         filename = QFileDialog.getOpenFileName(self, "Open File", os.getcwd())
 
         if filename[0]:
-            print(f"Opening {filename[0]}")
+            logging.info(f"Opening {filename[0]}")
 
         url = QUrl.fromLocalFile(filename[0])
 
         if url.isValid():
-            print(f"Using {url}")
+            logging.info(f"Using {url}")
             self.showfile(url)
 
     def showfile(self, url: QUrl):
-        self.ui.textBrowser.setOpenExternalLinks(True)
-        self.ui.textBrowser.setDocumentTitle(url.fileName())
+        self.textBrowser.setOpenExternalLinks(True)
+        self.textBrowser.setDocumentTitle(url.fileName())
         self.setWindowTitle(url.fileName())
-        self.ui.textBrowser.setSource(url, QTextDocument.MarkdownResource)
+        self.textBrowser.setSource(url, QTextDocument.MarkdownResource)
 
-        # print(f"Read only = {self.ui.textBrowser.isReadOnly()}")
+        logging.debug(f"Read only = {self.textBrowser.isReadOnly()}")
 
 
 if __name__ == "__main__":
+
+    # Catch CTL+C
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    parser = argparse.ArgumentParser(description="MKV - MarkDownViewer")
+    parser.add_argument(
+        "filename", metavar="file", type=str, nargs="?", help="file to open"
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        choices=["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"],
+        help="debug level",
+        default="WARNING",
+    )
+    args = parser.parse_args()
+
+    if args.filename:
+        filename_qurl = QUrl.fromLocalFile(args.filename)
+        if not filename_qurl.isValid():
+            sys.stderr.write(f"Invalid filename {args.filename}")
+            sys.exit(1)
+    else:
+        filename_qurl = None
+
     app = QApplication(sys.argv)
-
-    window = MainWindow()
-    window.show()
-
-    sys.exit(app.exec())
+    window = MarkDownViewer(filename_qurl, args.debug)
+    app.exec()
